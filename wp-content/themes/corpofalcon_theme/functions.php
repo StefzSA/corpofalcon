935084fa44ae2457f2d88bc425d2279cce3f8da4
EngelBM <engelbm98@gmail.com>
2024-01-25 11:22:33 -0400

    Changed the local font for the correct one and added a true white color to tailwind config.

--- wp-content/themes/corpofalcon_theme/functions.php
+++ wp-content/themes/corpofalcon_theme/functions.php
@@ -7,7 +7,7 @@ function assets_load_header_top() {
 	wp_enqueue_style('tailwind-css', get_template_directory_uri() .'/resources/css/tailwind/output.css');
 	wp_enqueue_style('fontawesome6', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
 	wp_enqueue_style('Font Include', get_template_directory_uri() .'/resources/css/font-include.css');
-	wp_enqueue_style('Neue Montreal', 'https://fonts.cdnfonts.com/css/montreal');
+	wp_enqueue_style('Montreal', 'https://fonts.cdnfonts.com/css/montreal');
 }
 
 function assets_load_header_bottom() {
--- wp-content/themes/corpofalcon_theme/tailwind.config.js
+++ wp-content/themes/corpofalcon_theme/tailwind.config.js
@@ -37,6 +37,7 @@ module.exports = {
         500: "#292929",
       },
       white: "#FAFBF6",
+      twhite: "#FFFFFF",
       transparent: "transparent",
     },
   },
